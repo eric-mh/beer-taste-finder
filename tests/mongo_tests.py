399ac45fe731a7e9c1dd3456c0e55bfee835d32d
mpcls <mpcls@ZlPhoenixs-MacBook.local>
2017-10-18 10:12:43 -0700

    More mongo tests added

--- tests/mongo_tests.py
+++ tests/mongo_tests.py
@@ -14,3 +14,24 @@ class TestMongoLoader(unittest.TestCase):
         self.assertTrue(mongo_names.database in mongo_wrapper.client.database_names())
         self.assertTrue(mongo_names.collection in mongo_wrapper.database.collection_names())
 
+    def test_iter_db(self):
+        #Assumes test_filter_db works
+        filter = {'beer/style' : ['Rauchbier'],
+                  'beer/ABV' : ['5.00']}
+        mongo_wrapper = src.ratings_importer.MongoGenerator(filter)
+
+        counts = 1
+        for review in mongo_wrapper:
+            self.assertIsNotNone(review)
+            counts += 1
+
+        expected_counts = 64
+        self.assertEqual(counts, expected_counts)
+
+    def test_filter_db(self):
+        filter = {'beer/style' : ['Rauchbier', 'Hefeweizen'],
+                  'beer/ABV' : ['5.00']}
+        mongo_wrapper = src.ratings_importer.MongoGenerator(filter)
+
+        expected_counts = 4511 + 64
+        self.assertEqual(mongo_wrapper.count(),expected_counts)
